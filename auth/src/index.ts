65a497432609066f9b8107dcfd62fbe822b2dc33
mohitb07 <bmohit980@gmail.com>
2021-09-17 20:43:47 +0530

    mongodb connection and route not found middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import 'express-async-errors'
 import {json} from 'body-parser'
+import mongoose from 'mongoose'
 
-import {currentUserRouter} from './routes/current-user'
-import {signinRouter} from './routes/signin'
-import {signoutRouter} from './routes/signout'
-import {signupRouter} from './routes/signup'
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { signupRouter } from './routes/signup'
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express()
 app.use(json())
@@ -14,8 +17,24 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
+
+// Not Found route ( all for any request method)
+app.all('*', async(req, res) => {
+    throw new NotFoundError()
+})
+
 app.use(errorHandler)
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!!')
-})
+const start = async () => {
+    try{
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        console.log('Connected to mongodb')
+    }catch(err){
+        console.log('Error', err)
+    }
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!')
+    })
+}
+
+start()

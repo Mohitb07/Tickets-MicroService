1a0e5c59200f67003affe93dae3ff32aac9dfe9d
mohitb07 <bmohit980@gmail.com>
2021-09-17 20:44:33 +0530

    refactored with abstract classes

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,32 +1,14 @@
 import {Response, Request, NextFunction} from 'express'
 
-import {DatabaseConnectionError} from '../errors/database-connection-error'
-import {RequestValidationError} from '../errors/request-validation-error'
+import { CustomError } from '../errors/custom-error'
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
     
-    if(err instanceof RequestValidationError){
-        const formattedErors = err.errors.map(error => (
-            {
-                message: error.msg,
-                field: error.param
-            }
-        ))
-        return res.status(400).send({
-            errors: formattedErors
+    if(err instanceof CustomError){
+        return res.status(err.statusCode).send({
+            errors: err.serializeErrors()
         })
     }
-
-    if(err instanceof DatabaseConnectionError){
-        return res.status(500).send({
-            errors: [
-                {
-                    message: err.reason
-                }
-            ]
-        })
-    }
-    
     res.status(400).send({
         errors: [
             {

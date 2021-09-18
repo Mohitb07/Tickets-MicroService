7a3bd8b66adce315932747aef61438afee090f3d
mohitb07 <bmohit980@gmail.com>
2021-09-18 21:27:53 +0530

    User model with typescript's interfaces(requirements)

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,10 +1,24 @@
 import mongoose from 'mongoose'
+import { Password } from '../services/password';
 
 interface UserAttrs {
     email: string,
     password: string
 }
 
+// An interface that describes the properties
+// that a User model has
+
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties 
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -17,10 +31,20 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-const User = mongoose.model('User', userSchema)
-// We are gonna use bulidUser to create user
-const buildUser = (attrs: UserAttrs) => {
+userSchema.pre('save', async function(done){
+    const user = this;
+    if(user.isModified('password')){
+        const hashed = await Password.toHash(this.get('password'))
+        user.set('password', hashed)
+    }
+    done()
+})
+
+userSchema.statics.build = (attrs: UserAttrs) => {
     return new User(attrs)
 }
 
-export {User, buildUser}
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
+
+
+export {User}

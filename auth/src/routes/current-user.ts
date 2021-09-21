bb2b7efc09387e27f9875a32662104a38459a490
mohitb07 <bmohit980@gmail.com>
2021-09-21 21:01:42 +0530

    get current logged in user info route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 
+import { currentUser } from '../middlewares/current-user'
+
 const router = express.Router()
 
-router.get('/api/user/currentuser', (req, res) => {
-    res.send('hi')
+router.get('/api/users/currentuser',currentUser, (req, res) => {
+    res.send({currentUser: req.currentUser || null})
 })
 
 export { router as currentUserRouter}

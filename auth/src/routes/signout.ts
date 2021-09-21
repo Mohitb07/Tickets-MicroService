eea59cf552f15c4f5002e7ab2ab7ae851fc24372
mohitb07 <bmohit980@gmail.com>
2021-09-21 21:02:42 +0530

    signout route

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,8 +2,9 @@ import express from 'express'
 
 const router = express.Router()
 
-router.post('/api/user/signout', (req, res) => {
-    res.send('hi')
+router.post('/api/users/signout', (req, res) => {
+    req.session = null;
+    res.send({})
 })
 
 export { router as signoutRouter}

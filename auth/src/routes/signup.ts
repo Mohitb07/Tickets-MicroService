c318505b8814fe1df87e3c76699023a6540fc137
mohitb07 <bmohit980@gmail.com>
2021-09-21 21:04:10 +0530

    code refactored, generated jwt and stored it on session

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,29 +1,26 @@
 import express, {Request, Response} from 'express'
-import { body, validationResult } from 'express-validator'
+import { body } from 'express-validator'
+import jwt from 'jsonwebtoken'
 
-import {RequestValidationError} from '../errors/request-validation-error'
+import { validateRequest } from '../middlewares/validate-request'
 import { BadRequestError } from '../errors/bad-request-error'
 import { User } from '../models/user'
 
 
 const router = express.Router()
 
-router.post('/api/users/signup',[
-    body('email')
-    .isEmail()
-    .withMessage('Email must be a valid email address'),
-    body('password')
-    .trim()
-    .isLength({min: 4, max: 20})
-    .withMessage('Password must be between 4 and 20 characters')
-],
+router.post('/api/users/signup',
+    [
+        body('email')
+        .isEmail()
+        .withMessage('Email must be a valid email address'),
+        body('password')
+        .trim()
+        .isLength({min: 4, max: 20})
+        .withMessage('Password must be between 4 and 20 characters')
+    ],
+    validateRequest,
     async(req: Request, res: Response) => {
-        const errors = validationResult(req)
-
-        if(!errors.isEmpty()){
-            throw new RequestValidationError(errors.array())
-        }
-
         const {email, password} = req.body;
 
         const existingUser = await User.findOne({email})
@@ -35,6 +32,17 @@ router.post('/api/users/signup',[
         const user = User.build({email, password})
         await user.save()
 
+        // GENERATE JWT
+        const userJwt = jwt.sign({
+            id: user.id,
+            email: user.email
+        }, process.env.JWT_KEY!)
+
+        // STORE IT ON SESSION OBJECT
+        req.session = {
+            jwt: userJwt
+        }
+        
         res.status(201).send(user)
 })
 

2d7420de762c8430951ef4014d653984c3b1d39f
mohitb07 <bmohit980@gmail.com>
2021-09-26 20:42:23 +0530

    code refactored

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,16 +1,10 @@
 import request from 'supertest'
+import authHelper from '../../test/authHelper'
 
 import app from '../../app'
 
 it('responds with current logged in user\'s details ', async () => {
-    const signUpResponse = await request(app)
-        .post('/api/users/signup')
-        .send({
-            email: 'test@test.com',
-            password: 'password'
-        })
-        .expect(201)
-    const cookie = signUpResponse.get('Set-Cookie')
+    const cookie = await authHelper();
 
     const response = await request(app)
         .get('/api/users/currentuser')
@@ -19,4 +13,13 @@ it('responds with current logged in user\'s details ', async () => {
         .expect(200)
     
     expect(response.body.currentUser.email).toEqual('test@test.com')
+})
+
+it('responds with null if not authenticated', async () => {
+    const response = await request(app)
+        .get('/api/users/currentuser')
+        .send()
+        .expect(200)
+    
+    expect(response.body.currentUser).toEqual(null)
 })

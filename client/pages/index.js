1b3b52e08e26cbe0a317004a13b51927c4665738
mohitb07 <bmohit980@gmail.com>
2021-09-27 21:38:57 +0530

    Inital data loading(get current user)

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,13 @@
-export default () => {
-    return <h1>Landing page1</h1>
+import buildClient from "../api/buildClient"
+
+const LandingPage = ({currentUser}) => {
+    return currentUser ? <h1 className="container">You are signed in</h1> : <h1 className="container">You are not signed in</h1>
 }
+
+LandingPage.getInitialProps = async (context) => { 
+    const client = buildClient(context)
+    const {data} = await client.get('/api/users/currentuser')
+    return data;
+}
+
+export default LandingPage;

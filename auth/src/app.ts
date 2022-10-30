46ca768037f7ba4995bc51af59ff91e2848e1dfd
mohitb07 <bmohit980@gmail.com>
2022-10-30 22:26:27 +0530

    code refactored

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import 'express-async-errors'
-import {json} from 'body-parser'
+// import {json} from 'body-parser'
 import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user'
@@ -11,7 +11,7 @@ import { errorHandler, NotFoundError } from '@mohittickdev/common';
 
 const app = express()
 app.set('trust proxy', true) // Trust the proxy for secure connection (ingress-nginx) in our case;
-app.use(json())
+app.use(express.json())
 app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'   // Only HTTPS connections allowed
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose'
+import {connect} from 'mongoose';
 import app from './app'
 
 const start = async () => {
@@ -6,7 +6,7 @@ const start = async () => {
         throw new Error('JWT_KEY must be defined')
     }
     try{
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        await connect('mongodb://auth-mongo-srv:27017/auth')
         console.log('Connected to mongodb')
     }catch(err){
         console.log('Error', err)
